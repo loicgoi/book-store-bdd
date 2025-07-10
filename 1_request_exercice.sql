4cbfd54bd70c2f2e8a9793f800019dae9da94749
[loicgoi] <g.loic.cours@gmail.com>
2025-07-10 11:56:04 +0200

    Réalisation exercice 2

--- 1_request_exercice.sql
+++ 1_request_exercice.sql
@@ -1,19 +1,28 @@
 -- 1. Afficher prénom et nom de tous les auteurs
+-- On utilise SELECT pour choisir les colonnes à afficher.
+-- FROM indique la table d'où proviennent les données.
 SELECT first_name, last_name
 FROM author;
 
 -- 2. Afficher auteurs nés entre 1950 et 1980
+-- Avec WHERE, on filtre les résultats selon une condition.
+-- On borne avec BETWEEN et AND pour inclure les années 1950 et 1980.
 SELECT first_name, last_name
 FROM author
 WHERE birth_year BETWEEN 1950 AND 1980;
 
 -- 3. Afficher 3 clients les plus récents (par date d’inscription)
+-- On utilise ORDER BY pour trier, ici on veut les résultats par date d'inscription.
+-- LIMIT 3 permet de ne garder que les 3 premiers résultats.
+-- On utilise DESC pour trier par ordre décroissant, donc les plus récents en premier
 SELECT email, join_date
 FROM customer
 ORDER BY join_date DESC
 LIMIT 3;
 
 -- 4. Liste commandes avec id et montant total renommé "Montant"
+-- On utilise AS pour renommer une colonne dans le résultat.
+-- Ici, on renomme total_amount en "Montant" pour une meilleure lisibilité.
 SELECT order_id, total_amount 
 AS "Montant"
 FROM customer_order;
@@ -29,17 +38,22 @@ FROM review
 WHERE rating >= 4;
 
 -- 7. Titres des livres avec année de publication, tri décroissant par année
+-- le mot-clé DESC permet de trier par ordre décroissant.
+-- Si on voulait l'inverse, on utiliserait ASC.
 SELECT title, publication_year
 FROM book
 ORDER BY publication_year DESC;
 
 -- 8. Commandes au mois de mars 2024
+-- On utilise le format 'YYYY-MM-DD' pour les dates.
 SELECT order_id, order_date
 FROM customer_order
 WHERE order_date BETWEEN '2024-03-01' AND '2024-03-31';
 
 
 -- 9. Livres dont le titre contient "secret"
+-- symbole % avant ou après le mot pour indiquer qu'il peut y avoir d'autres caractères.
+-- Sans ça, le 'secret' serait recherché exactement.
 SELECT title
 FROM book
 WHERE title LIKE '%secret%';
--- 2_request_exercice.sql
+++ 2_request_exercice.sql
@@ -1,23 +1,53 @@
 -- 1. Affichez la somme totale des stocks de livres.
-
+SELECT SUM(stock) AS total_stock
+FROM book;
 
 -- 2. Affichez pour chaque auteur le nombre de livres écrits.
-
+SELECT a.first_name, a.last_name, COUNT(b.book_id) AS nb_books
+FROM author a
+JOIN book b ON a.author_id = b.author_id
+GROUP BY a.author_id;
 
 -- 3. Affichez le titre du livre, nom complet de l’auteur, et nom du thème.
-
+SELECT b.title, CONCAT(a.first_name, ' ',a.last_name) AS full_name, t.theme_name
+FROM book b
+JOIN author a ON a.author_id = b.author_id
+JOIN theme t ON t.theme_id = b.theme_id;
 
 -- 4. Affichez toutes les commandes d’un client donné avec titre du livre et date de commande.
-
+SELECT co.order_id, b.title, co.order_date
+FROM customer_order co
+JOIN book b ON b.book_id = co.book_id
+JOIN customer c ON c.customer_id = co.customer_id
+WHERE c.customer_id = 4;
 
 -- 5. Affichez la moyenne des notes pour chaque livre ayant au moins 3 avis.
+SELECT b.title, AVG(r.rating) AS average_rating
+FROM review r
+JOIN book b ON b.book_id = r.book_id
+GROUP BY b.title
+HAVING COUNT(r.review_id) >= 3;
 
 
 -- 6. Affichez le nombre total de commandes passées par chaque client (email+nb commandes.
-
+SELECT c.email, COUNT(co.order_id) AS total_order
+FROM customer c
+LEFT JOIN customer_order co ON co.customer_id = c.customer_id
+GROUP BY c.customer_id
 
 -- 7. Affichez les titres des livres qui n’ont jamais été commandés.
-
+SELECT b.title, COUNT(co.order_id) AS total_order
+FROM book b
+LEFT JOIN customer_order co ON b.book_id = co.book_id
+GROUP BY b.book_id
+HAVING COUNT(co.order_id) = 0;
 
 -- 8. Affichez le chiffre d’affaires total généré par chaque auteur (somme des prix des livres commandés).
-
+SELECT 
+  CONCAT(a.first_name, ' ', a.last_name) AS full_name,
+  COALESCE(SUM(co.total_amount), 0) AS total_revenue
+FROM author a
+LEFT JOIN book b ON b.author_id = a.author_id
+LEFT JOIN customer_order co ON co.book_id = b.book_id
+GROUP BY a.author_id
+ORDER BY total_revenue DESC;
